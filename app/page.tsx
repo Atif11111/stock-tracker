99a5db047b45f5c5fa00c0abd7b5306aee210a33
Atif11111 <atifali19752003@gmail.com>
2026-05-26 12:18:10 -0400

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@base-ui/react"
+
+const Home: () => React.JSX.Element = () => {
+  return (
+    <div className = "flex items-center justify-center h-screen">
+      <Button>Click Me</Button>
+    </div>
+  )
+}
+
+export default Home
