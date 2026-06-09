1faf6654a2bcf887501bc1d8c0863df9eff022a2
Atif11111 <atifali19752003@gmail.com>
2026-06-09 12:18:21 -0400

    Edited nav

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -2,6 +2,7 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { NAV_ITEMS } from '../lib/constants';
+import SearchCommand from './SearchCommand';
 
 const NavItems = () => {
   const pathname = usePathname();
@@ -18,14 +19,21 @@ const NavItems = () => {
     <ul className="flex flex-col sm:flex-row gap-6 p-2 sm:gap-10 font-medium center-ed">
       {NAV_ITEMS.map((item) => (
         <li key={item.href}>
-          <Link
-            href={item.href}
-            className={`text-gray-400 hover:text-gray-200 transition-colors duration-200 ${
-              isActive(item.href) ? 'text-white' : ''
-            }`}
-          >
-            {item.label}
-          </Link>
+          {item.label === 'Search' ? (
+            <SearchCommand
+              renderAs="text"
+              label={item.label}
+            />
+          ) : (
+            <Link
+              href={item.href}
+              className={`text-gray-400 hover:text-gray-200 transition-colors duration-200 ${
+                isActive(item.href) ? 'text-white' : ''
+              }`}
+            >
+              {item.label}
+            </Link>
+          )}
         </li>
       ))}
     </ul>
